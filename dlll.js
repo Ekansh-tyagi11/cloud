0340eb208955a566484d27b8ee247a8984b77441
Ekansh.tyagi11 <ekanshtyagi9968@gmail.com>
2022-03-19 11:36:35 +0530

    first commit

---
+++ dlll.js
@@ -0,0 +1,14 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if(username=="Ekansh"&& password=="2001")
+{
+alert("Welcome");
+return false;
+}
+else
+{
+alert("Terrorist Terrorist");
+}
+}
